66b7601d17eedeedf03d6d39a28c724421c43ede
cexbrayat <cedric@ninja-squad.com>
2017-01-18 16:46:20 +0100

    feat: repro router issue in test

--- src/app/user/user.component.spec.ts
+++ src/app/user/user.component.spec.ts
@@ -1,14 +1,13 @@
 /* tslint:disable:no-unused-variable */
 
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppModule } from '../app.module';
 import { UserComponent } from './user.component';
 
 describe('Component: Users', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [AppModule, RouterTestingModule],
+      imports: [AppModule],
     });
   });
 
